746954f8cbd5cf57652b4e40d6b91ca78bad5501
Omri J. Sharon <omri_s@yahoo.com>
2022-08-18 21:32:33 +0300

    Update tk_widget_dnd.py

--- tk_widget_dnd.py
+++ tk_widget_dnd.py
@@ -2,16 +2,19 @@ from tkinter import CENTER
 
 
 def get_widgets(variables):
-    return {k: v for k, v in variables.items() if "tkinter" in str(type(v)).lower() and str(type(v)).find("Var")==-1}
+    return {k: v for k, v in variables.items() if "tkinter" in str(type(v)).lower() and hasattr(v, "bind")}
 
 
 def dnd(variables, anchor=CENTER, mouse_button=1):
     assert mouse_button in [1, 2, 3], "mouse_button must be 1, 2 or 3"
     valid_widgets = get_widgets(variables)
     assert len(valid_widgets) > 0, "No valid widgets found. variables argument should be equal to locals() or globals(), depending on your scope."
-    [v.bind("<B{}-Motion>".format(mouse_button), lambda event: event.widget.place(x=-event.widget.master.winfo_x() + event.x_root,
-                                                            y=-event.widget.master.winfo_y() + event.y_root,
-                                                            anchor=anchor)) for k, v in valid_widgets.items()]
+    [v.bind("<B{}-Motion>".format(mouse_button),
+            lambda event: event.widget.place(
+                x=-event.widget.master.winfo_x() + event.x_root,
+                y=-event.widget.master.winfo_y() + event.y_root,
+                anchor=anchor
+            )) for k, v in valid_widgets.items()]
 
 
 def get_widgets_position(variables):
@@ -22,3 +25,11 @@ def get_widgets_position(variables):
 def set_widgets_position(variables, widgets_position):
     for k, v in widgets_position.items():
         variables[k].place(x=v[0], y=v[1])
+        
+        
+def quit_wrapper(variables, quit_function, save_function, path, *args, **kwargs):
+    def wrapper():
+        widgets_position = get_widgets_position(variables)
+        save_function(widgets_position, path)
+        quit_function(*args, **kwargs)
+    return wrapper
